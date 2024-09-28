173f128530a840eb2f8383b70574be1d65476aa5
Aliaksandr Yafimchyk <efimchiksasha@gmail.com>
2024-09-28 19:41:13 +0300

    [build]: fix make in container issue

--- compile.py
+++ compile.py
@@ -9,16 +9,16 @@ DOCKER_IMAGE = "ghcr.io/fcsan-bsuir/bsuir_tex:main"
 def main():
     system = platform.system()
 
-    compile_cmd = 'make -j4 -C "src" all'
+    compile_cmd = "make -j4 -C 'src' all"
 
-    clean_cmd = 'make -C "src" clean'
+    clean_cmd = "make -C 'src' clean"
     
     docker_platform_flag = '--platform linux/amd64' if sysconfig.get_platform().split("-")[-1].lower() == 'arm64' else ''
-    run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test {DOCKER_IMAGE}'
+    run_docker_cmd = f'docker run {docker_platform_flag} -i --rm -v "{os.getcwd()}:/test" -w /test {DOCKER_IMAGE} sh -c'
 
     shell_and_symbol = ";" if system == "Windows" else "&&"
 
-    cmd = " ".join([run_docker_cmd, compile_cmd, shell_and_symbol, clean_cmd])
+    cmd = " ".join([run_docker_cmd, "\"", compile_cmd, "&&" , clean_cmd, "\""])
 
     print(f"Running command:\n{cmd}")
 
